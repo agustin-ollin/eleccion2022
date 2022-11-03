15ce8fbd9e3f3ee27e3aa5e7796c55317c4d3c61
Agustín Ollín García González <ollingonzalez37@gmail.com>
2022-11-03 17:51:17 -0600

    visualización de imágenes y pdf

--- public/js/custom.js
+++ public/js/custom.js
@@ -34,3 +34,69 @@ function validateData() {
     }
     return isValid;
 }
+
+
+var MAX_SIZE = 2048;
+var ONE_MB = 1000000;
+
+let loadImage = function (e) {
+    let img = document.getElementById("out-img");
+    img.src = URL.createObjectURL(event.target.files[0])
+}
+
+//Previsualizar la imagen
+let loadImg = () => {
+    let a = document.getElementById("foto").files[0].size;
+    a = (a / 1024)
+    console.log(a);
+    if (a > MAX_SIZE) {
+        alert("Imagen muy grande, tamaño actual " + a);
+        //setear a null la eleccion
+        document.getElementById('foto').value = null;
+        // setear a null la imagen, en caso de que se haya elegido una anterior
+        document.getElementById("out-img").style.display = 'none';
+    } else {
+        alert("Imagen aceptable ");
+        // obtiene el id de la imagen
+        let img = document.getElementById("out-img");
+
+        //Visualizamos la imagen
+        var archivo = document.getElementById("foto").files[0];
+        var reader = new FileReader();
+        if (foto) {
+            reader.readAsDataURL(archivo);
+            reader.onloadend = function () {
+                document.getElementById("img").src = reader.result;
+            }
+        }
+    }
+}
+
+
+
+//Previsualizar el pdf
+let loadFile = () => {
+    //Obtener el file
+    let a = document.getElementById("perfil").files[0].size;
+    //Dividir para tener una relacion con el tamaño de php.ini -> 2M
+    a = (a / 1024)
+    console.log(a);
+    if (a > MAX_SIZE) {
+        alert("Imagen muy grande, tamaño actual " + a + "MB");
+        //setear a null la eleccion
+        document.getElementById('perfil').value = null;
+    } else {
+        alert("Archivo aceptable ");
+
+        //visualizamos el pdf
+        var archivo = document.getElementById("perfil").files[0];
+        var reader = new FileReader();
+        if (perfil) {
+            reader.readAsDataURL(archivo);
+            reader.onloadend = function () {
+                document.getElementById("vistaPrevia").src = reader.result;
+            }
+        }
+    }
+
+}
--- resources/views/candidato/create.blade.php
+++ resources/views/candidato/create.blade.php
@@ -37,23 +37,92 @@
                     <label for="foto">Foto:</label>
                     <input type="file" id="foto" name="foto"
                            accept="image/png, image/jpeg"
-                           onchange="preview(event,'previewImage');"
+                           onchange="loadImg()"
                     >
                     <div id="previewImage"></div>
+                    <img id="img"  height="500px"/>
                 </div>
                 <div class="form-group">
                     <label for="perfil">Perfil:</label>
                     <input type="file" id="perfil" name="perfil"
                            accept="application/pdf"
-                           onchange="preview(event,'previewPDF');"
+                           onchange="loadFile()"
                     >
-                    <div id="previewPDF"></div>
+                    <div>
+                        <embed id="vistaPrevia" type="application/pdf" width="700">
+                    </div>
                 </div>
                 <button type="submit" class="btn btn-primary">Guardar</button>
             </form>
         </div>
     </div>
-    <script type="text/javascript"
+    <!--<script type="text/javascript"
             src="{{ URL::asset('js/custom.js') }}">
+    </script>-->
+    <script>
+        var MAX_SIZE = 2048;
+        var ONE_MB = 1000000;
+
+        let loadImage = function (e) {
+            let img = document.getElementById("out-img");
+            img.src = URL.createObjectURL(event.target.files[0])
+        }
+
+        //Previsualizar la imagen
+        let loadImg = () => {
+            let a = document.getElementById("foto").files[0].size;
+            a = (a / 1024)
+            console.log(a);
+            if (a > MAX_SIZE) {
+                alert("Imagen muy grande, tamaño actual " + a);
+                //setear a null la eleccion
+                document.getElementById('foto').value = null;
+                // setear a null la imagen, en caso de que se haya elegido una anterior
+                document.getElementById("out-img").style.display = 'none';
+            } else {
+                alert("Imagen aceptable ");
+                // obtiene el id de la imagen
+                let img = document.getElementById("out-img");
+
+                //Visualizamos la imagen
+                var archivo = document.getElementById("foto").files[0];
+                var reader = new FileReader();
+                if (foto) {
+                    reader.readAsDataURL(archivo);
+                    reader.onloadend = function () {
+                        document.getElementById("img").src = reader.result;
+                    }
+                }
+            }
+        }
+
+
+
+        //Previsualizar el pdf
+        let loadFile = () => {
+            //Obtener el file
+            let a = document.getElementById("perfil").files[0].size;
+            //Dividir para tener una relacion con el tamaño de php.ini -> 2M
+            a = (a / 1024)
+            console.log(a);
+            if (a > MAX_SIZE) {
+                alert("Imagen muy grande, tamaño actual " + a + "MB");
+                //setear a null la eleccion
+                document.getElementById('perfil').value = null;
+            } else {
+                alert("Archivo aceptable ");
+
+                //visualizamos el pdf
+                var archivo = document.getElementById("perfil").files[0];
+                var reader = new FileReader();
+                if (perfil) {
+                    reader.readAsDataURL(archivo);
+                    reader.onloadend = function () {
+                        document.getElementById("vistaPrevia").src = reader.result;
+                    }
+                }
+            }
+
+        }
     </script>
 @endsection
